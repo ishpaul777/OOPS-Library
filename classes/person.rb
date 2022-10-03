5a270f83c2721b553b744ba4dc64dcbd18902fa8
Ishpaul Singh <ishpaul207@gmail.com>
2022-10-03 14:20:07 +0530

    fixed linter errors

--- classes/person.rb
+++ classes/person.rb
@@ -1,5 +1,5 @@
 class Person
-  def initialize(age, name = 'unknown', parent_permission = true)
+  def initialize(age, name = 'unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -7,7 +7,7 @@ class Person
   end
 
   def can_use_services?
-    is_of_age? || @parent_permission
+    of_age? || @parent_permission
   end
 
   # getters and setters
@@ -17,7 +17,7 @@ class Person
   # private method
   private
 
-  def is_of_age?
+  def of_age?
     @age >= 18
   end
 end
--- classes/teacher.rb
+++ classes/teacher.rb
@@ -1,7 +1,7 @@
 require './person'
 
 class Teacher < Person
-  def initialize(specialization, age, name = 'Unknown', parent_permission = true)
+  def initialize(specialization, age, name = 'Unknown', parent_permission: true)
     super(age, parent_permission, name)
     @specialization = specialization
   end
