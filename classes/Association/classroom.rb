98dbb0e74990e9626dd42458f844cfa9d6780edf
Ishpaul Singh <ishpaul207@gmail.com>
2022-10-06 00:44:41 +0530

    added relation b/w classroom and student

---
+++ classes/Association/classroom.rb
@@ -0,0 +1,20 @@
+=begin
+Create a class Classroom with the following:
+@label instance variable, should be initialized in the constructor.
+Setter and getter for @label (remember about attr_accessor).
+=end
+
+class Classroom
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    @students.push(student)
+    student.classroom = self
+  end
+
+  attr_accessor :label
+  attr_reader :students
+end
--- classes/student.rb
+++ classes/student.rb
@@ -1,13 +1,18 @@
 require_relative './person'
 
 class Student < Person
-  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
-    super(age, parent_permission, name)
+  def initialize(classroom, age, name = 'Unknown', parent_permission = true)
+    super(age, name, parent_permission)
     @classroom = classroom
+    add_classroom(classroom)
   end
 
   def play_hooky
     "¯\(ツ)/¯"
+  end
+
+  def add_classroom(classroom)
+    classroom.add_student(self)
   end
 
   attr_accessor :classroom
