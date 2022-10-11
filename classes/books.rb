2631ee3aacdddacf378008296267316b5d8094ad
onomeh <onwuanibeonome@gmail.com>
2022-10-11 15:32:53 +0000

    preserve books data

--- classes/books.rb
+++ classes/books.rb
@@ -11,6 +11,13 @@ class Books
 
   def get_books
     # add code here
+    books = JSON.parse(File.read('Data/books.json'))
+    # for every book create a new book from author an title
+    books.map do |book|
+      newBook = Book.new(book['title'], book['author'])
+      # newBook.rentals = book['rentals']
+      @list << newBook
+    end 
   end
 
   def list_all_books
